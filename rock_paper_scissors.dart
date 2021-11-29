e051166295d87118c9694606a7ca5f9d01ab1db7
Anand Jha <akjha96@gmail.com>
2021-11-30 00:42:50 +0530

    modified userInput to map to enum noves

--- rock_paper_scissors.dart
+++ rock_paper_scissors.dart
@@ -5,29 +5,39 @@ import 'dart:math';
 enum Moves { rock, paper, scissor }
 void main() {
   while (true) {
+    var playerChoice;
     print('Rock, Paper or Scissor (r/p/s)');
     final playerInput = stdin.readLineSync();
 
     Random random = new Random();
     final aiChoice = Moves.values[random.nextInt(3)];
+    if (playerInput == 'r' || playerInput == 'p' || playerInput == 's') {
+      if (playerInput == 'r') {
+        playerChoice = Moves.rock;
+      } else if (playerInput == 'p') {
+        playerChoice = Moves.paper;
+      } else {
+        playerChoice = Moves.scissor;
+      }
+    }
 
-    if (playerInput == 'r' && aiChoice == Moves.paper) {
+    if (playerChoice == Moves.rock && aiChoice == Moves.paper) {
       print('Your Choice: Rock \nAI Choice: Paper\nYou Loose!\n');
-    } else if (playerInput == 'r' && aiChoice == Moves.scissor) {
+    } else if (playerChoice == Moves.rock && aiChoice == Moves.scissor) {
       print('Your Choice: Rock \nAI Choice: Scissor\nYou Win!\n');
-    } else if (playerInput == 'r' && aiChoice == Moves.rock) {
+    } else if (playerChoice == Moves.rock && aiChoice == Moves.rock) {
       print('Your Choice: Rock \nAI Choice: Rock\nIt\'s a Draw!\n');
-    } else if (playerInput == 'p' && aiChoice == Moves.rock) {
+    } else if (playerChoice == Moves.paper && aiChoice == Moves.rock) {
       print('Your Choice: Paper \nAI Choice: Rock\nYou Win!\n');
-    } else if (playerInput == 'p' && aiChoice == Moves.paper) {
+    } else if (playerChoice == Moves.paper && aiChoice == Moves.paper) {
       print('Your Choice: Paper \nAI Choice: Paper\nIt\'s a Draw!\n');
-    } else if (playerInput == 'p' && aiChoice == Moves.scissor) {
+    } else if (playerChoice == Moves.paper && aiChoice == Moves.scissor) {
       print('Your Choice: Paper \nAI Choice: Scissor\nYou Loose!\n');
-    } else if (playerInput == 's' && aiChoice == Moves.rock) {
+    } else if (playerChoice == Moves.scissor && aiChoice == Moves.rock) {
       print('Your Choice: Scissor \nAI Choice: Rock\nYou Loose!\n');
-    } else if (playerInput == 's' && aiChoice == Moves.paper) {
+    } else if (playerChoice == Moves.scissor && aiChoice == Moves.paper) {
       print('Your Choice: Scissor \nAI Choice: Paper\nYou Win!\n');
-    } else if (playerInput == 's' && aiChoice == Moves.scissor) {
+    } else if (playerChoice == Moves.scissor && aiChoice == Moves.scissor) {
       print('Your Choice: Scissor \nAI Choice: Scissor\nIt\'s a Draw!\n');
     } else if (playerInput == 'q') {
       print('Thanks for playing! Bye Bye now!!\n');
